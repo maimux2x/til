22fa98f571034572e0a7af6ea1621c2514d53fd1
maimux2x <m0smile10often@gmail.com>
2023-03-04 11:07:20 +0900

    ディレクトリ名の修正

---
+++ TypeScript/index-signature.ts
@@ -0,0 +1,13 @@
+interface Status {
+  [parameter: string]: number;
+}
+
+const myStatus: Status = {
+  level: 22,
+  experience: 3058,
+  maxHP: 156,
+  maxMP: 174,
+  attack: 39,
+  defence: 25,
+  job: 'maze'
+};
