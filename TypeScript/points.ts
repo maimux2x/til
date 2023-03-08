6e0b98bae9d54ac6adf130222f5392e9fb4c5b24
maimux2x <m0smile10often@gmail.com>
2023-03-08 23:47:07 +0900

    クラスをインターフェースとして扱う

---
+++ TypeScript/points.ts
@@ -0,0 +1,15 @@
+class Point {
+  x: number = 0;
+  y: number = 0;
+}
+
+const pointA = new Point();
+const pointB: Point = { x: 2, y: 4};
+
+interface Point3d extends Point {
+  z: number;
+}
+
+const pointC: Point3d = { x: 5, y: 5, z: 10 };
+
+console.log(pointC);
