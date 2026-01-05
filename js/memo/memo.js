6d6dcdc185c40358d1acbfc187a6c4caf5542fba
maimux2x <m0smile10often@gmail.com>
2026-01-05 20:51:46 +0900

    WIP: define MemoDatabase

--- js/memo/memo.js
+++ js/memo/memo.js
@@ -1,2 +1,35 @@
+import * as fs from 'node:fs';
+import sqlite3 from 'sqlite3';
 
+class MemoDatabase {
+  constructor(dbPath = ':memory:') {
+    this.db = new sqlite3.Database(dbPath);
+  }
 
+  init() {
+    this.db.run("CREATE TABLE memo (id INTEGER PRIMARY KEY, body TEXT NOT NULL)");
+  }
+
+  save(memo) {
+    this.db.run("INSERT INTO memo (body) VALUES (?)", memo);
+  }
+
+  close() {
+    this.db.close();
+  }
+}
+
+class Memo {
+  constructor() {
+    this.input = fs.readFileSync(0, 'utf8').trim();
+    this.database = new MemoDatabase();
+  }
+
+  add() {
+    this.database.init();
+    this.database.save(this.input);
+    this.database.close();
+  }
+}
+
+memo = new Memo();
