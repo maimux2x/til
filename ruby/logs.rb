4d6f6765dd200325ff9a85997f13bff58ac42aa3
maimux2x <m0smile10often@gmail.com>
2026-01-08 20:52:26 +0900

    Hash/Array practice

---
+++ ruby/logs.rb
@@ -0,0 +1,13 @@
+logs = [
+  { user_id: 1, status: "success" },
+  { user_id: 1, status: "failure" },
+  { user_id: 2, status: "success" },
+  { user_id: 2, status: "success" }
+]
+
+logs.each_with_object(Hash.new {|h, k| h[k] = {success: 0, failure: 0}}) {|object, h|
+  key    = object[:user_id]
+  status = object[:status].to_sym
+
+  h[key][status] += 1
+}
