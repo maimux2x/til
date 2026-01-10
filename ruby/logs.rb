a230174fc28fc386a82e42d26bfc095815f41271
maimux2x <m0smile10often@gmail.com>
2026-01-10 20:45:30 +0900

    update

--- ruby/logs.rb
+++ ruby/logs.rb
@@ -2,12 +2,14 @@ logs = [
   { user_id: 1, status: "success" },
   { user_id: 1, status: "failure" },
   { user_id: 2, status: "success" },
-  { user_id: 2, status: "success" }
+  { user_id: 2, status: "success" },
+  { user_id: 1, status: "pending" }
 ]
 
 logs.each_with_object(Hash.new {|h, k| h[k] = {success: 0, failure: 0}}) {|object, h|
   key    = object[:user_id]
   status = object[:status].to_sym
 
+  h[key][status] ||= 0
   h[key][status] += 1
 }
