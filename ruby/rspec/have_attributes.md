a24081e797d463b00d6740075491635529c5cdbf
maimux2x <m0smile10often@gmail.com>
2026-01-26 23:04:42 +0900

    More about have_attributes

--- ruby/rspec/have_attributes.md
+++ ruby/rspec/have_attributes.md
@@ -1,8 +1,19 @@
 ## have_attributes matcher
 
 オブジェクトの属性が期待される属性と一致することを指定できる
+オブジェクトのメソッドを見る matcher
 
 ```ruby
 person = Person.new("Jim", 32)
 expect(person).to have_attributes(:name => "Jim", :age => 32)
+```
+
+```ruby
+class Foo
+  def x = 'x'
+end
+
+foo = Foo.new
+
+expect(foo).to have_attributes(x: 'x')
 ```
--- ruby/ruby_box/say_hello_2.rb
+++ ruby/ruby_box/say_hello_2.rb
@@ -1,23 +1,23 @@
 require "./greeting"
 
-Greeting.new.say_hello
+# Greeting.new.say_hello
 
-Greeting.polite = true
-Greeting.new.say_hello
+# Greeting.polite = true
+# Greeting.new.say_hello
 
-t = Thread.new { Greeting.new.say_hello }
-Greeting.polite = false
-t.join
+# t = Thread.new { Greeting.new.say_hello }
+# Greeting.polite = false
+# t.join
 
-# b1 = Ruby::Box.new
-# b1.require "./greeting"
+b1 = Ruby::Box.new
+b1.require "./greeting"
 
-# b2 = Ruby::Box.new
-# b2.require "./greeting"
+b2 = Ruby::Box.new
+b2.require "./greeting"
 
-# b2::Greeting.polite = true
+b2::Greeting.polite = true
 
-# [
-#   Thread.new { b1::Greeting.new.say_hello },
-#   Thread.new { b2::Greeting.new.say_hello }
-# ].each(&:join)
+[
+  Thread.new { b1::Greeting.new.say_hello },
+  Thread.new { b2::Greeting.new.say_hello }
+].each(&:join)
